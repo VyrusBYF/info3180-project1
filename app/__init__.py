45ead32af0ad59f0645d7abc84328daf9d9aa8ae
Richard Howlett <richardhowlett14@gmail.com>
2020-04-14 15:41:17 -0400

    Procfile

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "change this to be a more random key"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://madploopgrgiol:e02d1aec3f58afe489f1f48a951866199ff69f61cd79c70d33b77a8056d21d44@ec2-34-204-22-76.compute-1.amazonaws.com:5432/d139behheo8d0r'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://qudzbqqlashgur:98a208eb1f38cd7edd45326b23f65d66e6389e0487608203d52330cb5efde536@ec2-34-197-212-240.compute-1.amazonaws.com:5432/d5enson5ggltov'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 db = SQLAlchemy(app)
